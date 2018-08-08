06c023b87e5cff5dae8f75b40eb0825864653777
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 10:27:54 +0300

    dshkd

--- exercises/app.py
+++ exercises/app.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-	return render_template("index.html")
+	fav_swimmers=["sarah","me", "sarah again"]
+	return render_template(
+		"index.html",fav_swimmers=fav_swimmers,
+		likes_same_sport=True)
+
+
     
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug = True, port=8080)
